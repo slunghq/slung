feb1409b65e7a6860b6e209b33198c34b370c842
notxorand <ewanretorokugbe@gmail.com>
2026-02-01 15:37:11 +0100

    chore: readying tsm tree

--- src/tsm/tsm.zig
+++ src/tsm/tsm.zig
@@ -1,12 +1,15 @@
 const std = @import("std");
 const table = @import("table.zig");
-const ColumnTable = table.ColumnTable;
 const cache = @import("cache.zig");
-const Cache = cache.Cache;
+const entry = @import("entry.zig");
 const Allocator = std.mem.Allocator;
+const Cache = cache.Cache;
+const ColumnTable = table.ColumnTable;
+const DiskEntry = entry.DiskEntry;
 
 
 test {
     _ = table;
     _ = cache;
+    _ = entry;
 }
